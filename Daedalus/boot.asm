e6b951636dda8a0459e32fd391e6e6462d397216
BonnieToGamer <filip.holmgren2@elev.ga.lbs.se>
2023-09-11 18:35:09 +0200

    Episode 2 challenge daedalus

--- Daedalus/boot.asm
+++ Daedalus/boot.asm
@@ -1,14 +1,29 @@
  mov    ah, 0x0e
  mov    al, 64
- 
+ mov    bh, 0xff
+
  loop:
+    not bh
     inc al
     
     cmp al, 'Z' + 1 ; if al == 'Z' + 1
     je  halt        ; goto halt
 
+    cmp bh, 0
+    je add
+    jmp remove
+
+print:
     int 0x10
     jmp loop
+
+add:
+    add al, 32
+    jmp print
+
+remove:
+    sub al, 32
+    jmp print
 
 halt:
     jmp halt
