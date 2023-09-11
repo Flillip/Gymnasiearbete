4a2474e86cc50388af3755f05b345175216a84aa
BonnieToGamer <filip.holmgren2@elev.ga.lbs.se>
2023-09-11 18:50:37 +0200

    Episode 3 daedalus

--- Daedalus/boot.asm
+++ Daedalus/boot.asm
@@ -1,32 +1,22 @@
- mov    ah, 0x0e
- mov    al, 64
- mov    bh, 0xff
+ [org 0x7c00]   ; mem origin point is 0x7c00
+ mov    ah, 0x0e; set mode to TTY
+ mov    bx, str ; moves the mem address of str into bx
 
  loop:
-    not bh
-    inc al
+    mov al, [bx]; get next char in string
     
-    cmp al, 'Z' + 1 ; if al == 'Z' + 1
-    je  halt        ; goto halt
-
-    cmp bh, 0
-    je add
-    jmp remove
-
-print:
-    int 0x10
-    jmp loop
-
-add:
-    add al, 32
-    jmp print
-
-remove:
-    sub al, 32
-    jmp print
+    cmp al, 0   ; if al == 0
+    je  halt    ; goto halt
+    
+    int 0x10    ; interrupt 0x10, print
+    inc bx      ; incremeant bx pointer
+    jmp loop    ; loop
 
 halt:
     jmp halt
+
+str:
+    db "Goodbye, world", 0
 
  times 510-($-$$) db 0
  db 0x55, 0xaa
