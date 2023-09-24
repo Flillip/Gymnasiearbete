ef3358f23fe14401a2affdeb7b0bf385328cf037
Flillip <119044858+Flillip@users.noreply.github.com>
2023-09-24 02:00:10 +0200

    Video 7

--- Daedalus/boot.asm
+++ Daedalus/boot.asm
@@ -1,60 +1,86 @@
     [org 0x7c00]   ; mem origin point is 0x7c00
 
+    KERNEL_LOCATION equ 0x1000
+
+    BOOT_DISK db 0
     mov [BOOT_DISK], dl
+
+    xor ax, ax
+    mov es, ax
+    mov ds, ax
+    mov bp, 0x8000
+    mov sp, bp
+
+    mov bx, KERNEL_LOCATION
+    mov dh, 20
+
+    mov ah, 0x02
+    mov al, dh
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+    mov dl, [BOOT_DISK]
+    int 0x13
+
+    mov ah, 0x0
+    mov al, 0x3
+    int 0x10
 
     CODE_SEG equ GDT_code - GDT_start
     DATA_SEG equ GDT_data - GDT_start
 
     cli
     lgdt [GDT_descriptor]
-    ; change last bit of cr0 to 1
     mov eax, cr0
     or eax, 1
     mov cr0, eax
-
-    ; far jump (jmp to other segment)
     jmp CODE_SEG:start_protected_mode
 
+    jmp $
 
 GDT_start:
     GDT_null:
-        dd 0x0 ; four times 00000000
-        dd 0x0 ; four times 00000000
-    
+        dd 0x0
+        dd 0x0
+
     GDT_code:
-        dw 0xffff ; first 16 bits of limit
-        dw 0x0 ; 16 bits +
-        db 0x0 ; 8 bits = 24
-        db 0b10011010 ; present, privelage, type, Type flags
-        db 0b11001111 ; Other + limit (last four bits)
-        db 0x0 ; Last 8 bits of base
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
 
     GDT_data:
-        dw 0xffff ; first 16 bits of limit
-        dw 0x0 ; 16 bits +
-        db 0x0 ; 8 bits = 24
-        db 0b10010010 ; present, privelage, type, Type flags
-        db 0b11001111 ; Other + limit (last four bits)
-        db 0x0 ; Last 8 bits of base
-    
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
+
 GDT_end:
 
 GDT_descriptor:
-    dw GDT_end - GDT_start - 1 ; size
-    dd GDT_start               ; start
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
+
 
 [bits 32]
 start_protected_mode:
-    mov al, 'A'
-    mov ah, 0x0f
-    mov [0xb8000], ax
+    mov ax, DATA_SEG
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	mov ebp, 0x90000		; 32 bit stack base pointer
+	mov esp, ebp
 
-    jmp halt
+    jmp KERNEL_LOCATION
 
-BOOT_DISK: db 0
-
-halt:
-    jmp halt
+    
 
     times 510-($-$$) db 0
     db 0x55, 0xaa
--- Daedalus/build.sh
+++ Daedalus/build.sh
@@ -1,2 +1,20 @@
-nasm ./boot.asm -f bin -o ./boot.bin
-qemu-system-i386 -hda boot.bin -monitor stdio 
+# nasm ./boot.asm -f bin -o ./boot.bin
+# qemu-system-i386 -hda boot.bin -monitor stdio 
+
+mkdir ./bin
+cd ./bin
+
+nasm "../boot.asm" -f bin -o "boot.bin"
+nasm "../kernel_entry.asm" -f elf -o "kernel_entry.o"
+nasm "../zeroes.asm" -f bin -o "zeroes.bin"
+
+
+i386-elf-gcc -ffreestanding -m32 -g -c "../kernel.cpp" -o "kernel.o"
+i386-elf-ld -o "full_kernel.bin" -Ttext 0x1000 "kernel_entry.o" "kernel.o" --oformat binary
+
+cat "boot.bin" "full_kernel.bin" > "everything.bin"
+cat "everything.bin" "zeroes.bin" > "OS.bin"
+
+qemu-system-x86_64 -drive format=raw,file="OS.bin",index=0,if=floppy, -m 128M
+
+cd ..
